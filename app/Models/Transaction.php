cc77f2efe5e46234640d3e0608413049438ad6f5
Windy <saputrawindybintan@gmail.com>
2026-06-07 14:03:15 +0700

    transactions-1

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,10 +8,11 @@ class Transaction extends Model
 {
     use \App\Traits\BelongsToTenant;
 
-    protected $fillable = ['tenant_id', 'user_id', 'total_amount'];
+    protected $fillable = ['tenant_id', 'user_id', 'total_amount', 'customer_name', 'trans_date', 'number'];
 
     protected $casts = [
         'total_amount' => 'decimal:2',
+        'trans_date' => 'datetime',
     ];
 
     public function tenant()
